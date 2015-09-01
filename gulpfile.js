f5185a320fc0be51507e84e6afda82ee5af8eeb3
wm <thehobbitmime@gmail.com>
2015-08-31 21:57:14 -0400

    initial part 2

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/*
+Intent: build options
+*/
+
+var gulp = require('gulp');
+var gls = require('gulp-live-server');
+
+gulp.task('default',['start']);
+
+// "build" forces gulp to wait (its a dependency)
+gulp.task('start',function() {
+    var server = gls.new('server.js');
+    server.start(); // .run(['./dist/server/app.js']);    
+    
+    gulp.watch('./www/index.html',function() {
+        server.notify.apply(server,arguments);
+    });
+    // TODO: combine these
+    
+    gulp.watch('./www/**/*.js',function() {
+        server.notify.apply(server,arguments);
+    });
+    
+       
+    
+    
+});
+
